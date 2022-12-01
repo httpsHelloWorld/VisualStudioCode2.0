1def3201f417394bd1507bcccf220276125cd97c
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-11-30 23:54:20 -0500

    database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -20,9 +20,12 @@ export const putDb = async (content) => {
   const request = store.put({jate:content});
   const result = await request;
   console.log('Data stored', result);
-}
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // TODO: Add logic for a method that gets all the content from the database
+  const jateDB = await openDB('jate',1);
+
+}
 
 initdb();
