9567ccb05e52d58a309c79809b0cda22f0974a4b
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-11-30 22:00:58 -0500

    TODO: Implement asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,15 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  // TODO: Implement asset caching
+  ({request}) => ['style','script','worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName:'asset-cache',
+    plugins:[
+      new CacheableResponsePlugin({
+        statuses:[0,200]
+      })
+    ]
+  })
+);
