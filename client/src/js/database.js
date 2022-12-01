aba1167e15eb2df9837e44643b3d71592d3790cf
Hasan Noor <httpshelloworld@hasans-imac.mynetworksettings.com>
2022-11-30 23:52:15 -0500

    TODO: Add logic to a method that accepts some content and adds it to the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,15 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // TODO: Add logic to a method that accepts some content and adds it to the database
+  const jateDB = await openDB('jate',1);
+  const transaction = jateDB.transaction('jate','readwrite');
+  const store = transaction.objectStore('jate');
+  const request = store.put({jate:content});
+  const result = await request;
+  console.log('Data stored', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
